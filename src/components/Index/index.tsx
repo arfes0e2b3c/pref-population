42db4754c24cd13ce52b4a9e450c5cd076a06a24
arfes0e2b3c <webyata@gmail.com>
2023-04-10 02:20:27 +0900

    ページ全体のスタイルを追加

--- src/components/Index/index.tsx
+++ src/components/Index/index.tsx
@@ -2,6 +2,7 @@ import { FC } from 'react';
 import PrefList from './elements/PrefList';
 import usePopulationList from './hooks/populationListHooks';
 import { PopulationChart } from './elements/PopulationChart';
+import { index, indexInner } from './styles/index.css';
 import { Prefecture } from '@/types/types';
 
 type Props = {
@@ -11,15 +12,17 @@ type Props = {
 export const Index: FC<Props> = ({ prefectureList }) => {
   let { populationList, fetchPopulationList } = usePopulationList();
   return (
-    <>
-      <PrefList
-        prefectureList={prefectureList}
-        onChange={checkedList => fetchPopulationList(checkedList)}
-      />
-      <PopulationChart
-        populationList={populationList}
-        prefectureList={prefectureList}
-      />
-    </>
+    <div className={index}>
+      <div className={indexInner}>
+        <PrefList
+          prefectureList={prefectureList}
+          onChange={checkedList => fetchPopulationList(checkedList)}
+        />
+        <PopulationChart
+          populationList={populationList}
+          prefectureList={prefectureList}
+        />
+      </div>
+    </div>
   );
 };
