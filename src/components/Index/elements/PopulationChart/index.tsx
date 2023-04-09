f89fe2c068296516a43c858a311d8c314f601788
arfes0e2b3c <webyata@gmail.com>
2023-04-10 02:25:01 +0900

    グラフのレスポンシブ対応、スタイルの追加

--- src/components/Index/elements/PopulationChart/index.tsx
+++ src/components/Index/elements/PopulationChart/index.tsx
@@ -1,7 +1,20 @@
 import { Prefecture } from '@/types/types';
 import { supplyStrokeColor } from '@/utils/colors';
 import { FC } from 'react';
+import {
+  LineChart,
+  Line,
+  Legend,
+  XAxis,
+  YAxis,
+  Tooltip,
+  ResponsiveContainer,
+} from 'recharts';
+import {
   customTooltip,
+  populationChart,
+  tooltipLabel,
+} from './styles/populationChart.css';
 type Population = any;
 type PopulationChartProps = {
   populationList: Population[];
@@ -32,7 +45,46 @@ export const PopulationChart: FC<PopulationChartProps> = ({
 }) => {
   return (
     <div className={populationChart}>
+      <ResponsiveContainer width='100%' height={400}>
+        <LineChart margin={{ top: 10, left: 10, right: 20 }}>
+          {populationList &&
+            populationList.map(population => {
+              return (
+                <Line
+                  data={population['data'][0]['data']}
+                  key={population['index']}
+                  type='basis'
+                  dataKey='value'
+                  legendType='plainline'
+                  dot={{ stroke: 'gray', strokeWidth: 4 }}
+                  activeDot={{ stroke: 'white', r: 5 }}
+                  stroke={supplyStrokeColor(population['index'])}
+                  strokeWidth={1.5}
+                  name={prefectureList[population['index'] - 1].prefName}
+                />
+              );
+            })}
+          <Legend
+            height={20}
+            layout='horizontal'
+            align='center'
+            verticalAlign='bottom'
+          />
+          <XAxis
+            type='number'
+            dataKey='year'
+            domain={[1960, 2045]}
+            tickCount={8}
+            stroke='#ddd'
+          />
+          <YAxis
+            stroke='#ddd'
+            tickCount={5}
+            tickFormatter={population => population / 10000 + '万'}
+          />
           <Tooltip content={<CustomTooltip />} />
+        </LineChart>
+      </ResponsiveContainer>
     </div>
   );
 };
--- src/components/Index/elements/PopulationChart/styles/populationChart.css.ts
+++ src/components/Index/elements/PopulationChart/styles/populationChart.css.ts
@@ -1,5 +1,21 @@
 import { style } from '@vanilla-extract/css';
 
 export const populationChart = style({
+  width: '100%',
   margin: '30px',
+  '@media': {
+    'screen and (max-width: 768px)': {
+      margin: '30px 0',
+    },
+  },
+});
+
+export const customTooltip = style({
+  padding: '20px 30px',
+  backgroundColor: 'rgba(0,0,0,0.5)',
+});
+
+export const tooltipLabel = style({
+  fontWeight: 'bold',
+  marginBottom: '10px',
 });
